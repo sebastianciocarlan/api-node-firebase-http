15e45cc301062210566ec5cf1ff45a34a5252e6b
sebastianciocarlan <sebastianciocarlan@gmail.com>
2022-08-12 00:53:56 +0200

    update index

--- functions/index.js
+++ functions/index.js
@@ -3,16 +3,30 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 app.use(cors({ origin: true }));
+const { initializeApp, cert } = require('firebase-admin/app');
+
+const { getFirestore} = require('firebase-admin/firestore');
+const privateKey = require('./private-key.json');
+
+initializeApp({
+    credential: cert(privateKey)
+});
 
 
+const db = getFirestore();
 
 app.get('/hello', (req, res) => {
-    res.end("He recibido una peticion GET");  
-  });
-  
-  app.post('/hello', (req, res) => {
-    res.end("He recibido una peticion POST");  
-  });
-  
-  // Expose Express API as a single Cloud Function:
-  exports.widgets = functions.https.onRequest(app);
+    res.end("He recibido una peticion GET");
+});
+
+app.post('/hello', (req, res) => {
+    db.collection('testing').add( req.body ).then(ref => {
+        res.end("Se han almacenado los datos")
+    }).catch(err => {
+        res.end(err)
+    });
+});
+
+
+
+exports.widgets = functions.https.onRequest(app);
